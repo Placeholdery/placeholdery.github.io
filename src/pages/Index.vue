9616654d18e6a810efbbd4378a4a30ba615ab6b4
Zonayed Ahmed <zonayedpca@yahoo.com>
2020-03-01 12:30:49 +0600

    file upload on click

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,15 +1,24 @@
 <template>
   <Layout>
-    <div class="uploader">
+    <div v-if="!files.length" class="uploader">
       <div>
         <image-icon size="10x" />
       </div>
       <div>
         <h2>Drag & Drop Image/s</h2>
         <p>or</p>
-        <input class="fileinput" type="file" multiple ref="fileinput" />
+        <input
+          @change="fileUpload"
+          class="fileinput"
+          type="file"
+          multiple
+          ref="fileinput"
+        />
         <button @click="upload">Upload</button>
       </div>
+    </div>
+    <div v-else class="upload-action">
+      Action
     </div>
   </Layout>
 </template>
@@ -27,10 +36,23 @@ export default {
   metaInfo: {
     title: "Homepage"
   },
+  data: () => {
+    return {
+      files: []
+    };
+  },
   methods: {
     upload: function() {
       const fileInput = this.$refs["fileinput"];
       fileInput.click();
+    },
+    fileUpload: function(e) {
+      const {
+        target: { files }
+      } = e;
+      if (files.length) {
+        this.files = files;
+      }
     }
   },
   components: {
